e2786c24d1b771d78b8e9b98ef000b1f3028191b
guanzhengfu <guanzheng.fu@thoughtworks.com>
2022-04-18 17:25:50 +0800

    test

---
+++ Jenkinsfile_ci.groovy
@@ -0,0 +1,74 @@
+pipeline {
+    agent any
+    triggers {
+        GenericTrigger(
+                genericVariables: [
+                        [key: 'ref', value: '$.ref']
+                ],
+                causeString: 'Triggered on $ref',
+                token: 'abc123',
+                printContributedVariables: true,
+                printPostContent: true,
+                silentResponse: false,
+                regexpFilterText: '$ref',
+        )
+    }
+    stages {
+        stage('Checkout') {
+            steps {
+                checkout([
+                        $class                           : 'GitSCM',
+                        branches                         : [[name: '*/master']],
+                        doGenerateSubmoduleConfigurations: false
+                ])
+            }
+        }
+
+        stage('Confirm qa') {
+            options {
+                timeout(time: 840, unit: 'SECONDS')
+            }
+
+            input {
+                message 'Do you want to delpoy to qa?'
+                ok 'Yes, go ahead.'
+            }
+
+            steps {
+                sh 'chmod 777 test.sh'
+                sh './test.sh'
+                echo 'qa'
+            }
+        }
+
+        stage('Confirm staging') {
+            options {
+                timeout(time: 840, unit: 'SECONDS')
+            }
+
+            input {
+                message 'Do you want to delpoy to staging?'
+                ok 'Yes, go ahead.'
+            }
+
+            steps {
+                echo 'staging'
+            }
+        }
+
+        stage('Confirm prod') {
+            options {
+                timeout(time: 840, unit: 'SECONDS')
+            }
+
+            input {
+                message 'Do you want to delpoy to prod?'
+                ok 'Yes, go ahead.'
+            }
+
+            steps {
+                echo 'prod'
+            }
+        }
+    }
+}
