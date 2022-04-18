a60c837a3ca9b0fcafd6c821aeb94cd012bbcbe5
guanzhengfu <guanzheng.fu@thoughtworks.com>
2022-04-18 18:06:54 +0800

    test

--- test.sh
+++ test.sh
@@ -35,6 +35,7 @@ cp  -r ${jar_path}/${jar_name} ${www_path}
 cd  ${www_path}
 #启动jar，指定SpringBoot的profiles为beta,后台启动
 #java -jar -Dspring.profiles.active=beta ${jar_name} &
+echo ${jar_name}
 java -jar ${jar_name} &
 #将进程ID存入到ufind-web.pid文件中
 echo $! > /home/wwwroot/jenkins/test3.pid
